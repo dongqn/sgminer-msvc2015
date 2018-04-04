5163398f525cc565943ef58d3d711418841f6997
dongqn <36226384+dongqn@users.noreply.github.com>
2018-04-05 01:33:03 +0700

    Update lyra2z330.c

--- algorithm/lyra2z330.c
+++ algorithm/lyra2z330.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "Lyra2z.h"
+#include "Lyra2Z.h"
 
 #define _ALIGN(x) __attribute__ ((aligned(x)))
 
@@ -13,7 +13,7 @@ void lyra2z330_hash(const char* input, char* output, uint32_t len)
 {
   uint32_t _ALIGN(64) hash[8];
 
-  LYRA2z((void*)hash, 32, (void*)input, len, (void*)input, len, 2, 330, 256);
+  LYRA2Z((void*)hash, 32, (void*)input, len, (void*)input, len, 2, 330, 256);
 
   memcpy(output, hash, 32);
 }
